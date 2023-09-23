88ed56b7ace82c37387e3ad6710e2b53f7089449
lainouzz <linuslind3@gmail.com>
2023-09-23 20:06:51 +0200

    FineTine_Movement
    A more Fine tune work on player fall physics

--- Assignment_Space_Lander/Assets/Scripts/PlayerBoost.cs
+++ Assignment_Space_Lander/Assets/Scripts/PlayerBoost.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boost : MonoBehaviour
+public class PlayerBoost : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +13,6 @@ public class Boost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-
-        }
+        
     }
 }
--- Assignment_Space_Lander/Assets/Scripts/PlayerRotate.cs
+++ Assignment_Space_Lander/Assets/Scripts/PlayerRotate.cs
@@ -1,30 +1,61 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Debug = UnityEngine.Debug;
 
 public class PlayerRotate : MonoBehaviour
 {
+    public float boostSpeed;
+    public float counterXForce;
+
+    private bool isPlayerBoosting;
+    public bool isGrounded;
+
+    public LayerMask GroundLayer;
+
     public Transform playerObj;
     public Camera mainCam;
-    public float speed;
-
     private Vector3 target;
+    private Vector2 distance;
     private Rigidbody2D rb2d;
+    private Quaternion originalRot;
 
     // Start is called before the first frame update
     void Awake()
     {
         mainCam = Camera.main;
         rb2d = GetComponent<Rigidbody2D>();
+        rb2d.gravityScale = 1f;
+        originalRot = transform.rotation;
     }
 
     // Update is called once per frame
     public void Update()
     {
+        UpdatePos();
+        Grounded();
+        if (Input.GetMouseButtonDown(0))
+        {
+            EnableBoost();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            DisableBoost();
+        }
+
+        if (isPlayerBoosting)
+        {
+            Boosting();
+        }
+    }
+
+    public void UpdatePos()
+    {
         if (playerObj != null)
         {
-            //Vector3 mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
             target = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
             Vector3 Dir = target - playerObj.transform.position;
@@ -33,19 +64,57 @@ public class PlayerRotate : MonoBehaviour
 
             angles = angles >= 45f ? 45 : (angles <= -45 ? -45f : angles);
 
-            transform.rotation = Quaternion.AngleAxis(-angles, Vector3.forward);
-
-            if (Input.GetKey(KeyCode.Mouse0))
+            if (!isGrounded)
             {
-                rb2d.gravityScale = 0;
-                transform.position = Vector3.MoveTowards(playerObj.position, target, speed * Time.deltaTime);
+                transform.rotation = Quaternion.AngleAxis(-angles, Vector3.forward);
+            }
+        }
+    }
+
+    public void EnableBoost()
+    {
+        isPlayerBoosting = true;
+        rb2d.gravityScale = 0f;
+        isGrounded = false;
+    }
+    public void DisableBoost()
+    {
+        isPlayerBoosting = false;
+        rb2d.gravityScale = 1f;
+
+        //Counter force on X axis to smooth it in;
+        Vector2 slowForce = -rb2d.velocity * counterXForce;
+        rb2d.AddForce(slowForce, ForceMode2D.Force);
+
+    }
+
+    public void Boosting()
+    {
+        Vector3 Direction = (target - transform.position);
+        rb2d.AddForce(Direction * boostSpeed);
+    }
+
+    public void Grounded()
+    {
+        if (!isGrounded)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, GroundLayer);
+            if (hit.collider != null && hit.distance <= 1f)
+            {
+                isGrounded = true;
+                rb2d.freezeRotation = true;
+                rb2d.gravityScale = 0f;
+                Debug.DrawRay(distance, Vector2.down, Color.green);
+                Debug.Log("Grounded");
 
             }
 
-
         }
-
+        else
+        {
+            transform.rotation = originalRot;
+        }
         
-
     }
+
 }
