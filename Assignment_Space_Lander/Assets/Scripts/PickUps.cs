aeca00da1f6428e0de1b63042b6997574e4807e1
Linus Lind <linus.lind@edu.futuregames.nu>
2023-09-27 14:52:03 +0200

    MainMenu
    test

---
+++ Assignment_Space_Lander/Assets/Scripts/PickUps.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUps : MonoBehaviour
+{
+    public GameObject activateShield;
+    private bool isShieldActive;
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(ActiveShieldTimer());
+        }
+    }
+
+    IEnumerator ActiveShieldTimer()
+    {
+        isShieldActive = true;
+        activateShield.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        isShieldActive = false;
+        activateShield.gameObject.SetActive(false);
+
+    }
+}
