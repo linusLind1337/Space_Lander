d90bee05601d5e6f445a44b10b8828309d9396a6
lainouzz <linuslind3@gmail.com>
2023-10-03 09:55:00 +0200

    BulletDestroy

---
+++ Assignment_Space_Lander/Assets/Scripts/BulletDestroy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assignment_Space_Lander/Assets/Scripts/Obstacles.cs
+++ Assignment_Space_Lander/Assets/Scripts/Obstacles.cs
@@ -17,7 +17,7 @@ public class Obstacles : MonoBehaviour
 
     private void Update()
     {   
-        if (Vector2.Distance(transform.position, player.position) < shootDist)
+        if (player != null && Vector2.Distance(transform.position, player.position) < shootDist)
         {
             // Check if enough time has passed since the last shot.
             if (Time.time - lastShotTimeCounter >= shootInterval)
@@ -33,6 +33,8 @@ public class Obstacles : MonoBehaviour
         // Instantiate and shoot a bullet towards the player.
         GameObject bullets = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
         Vector2 direction = (player.position - transform.position).normalized;
+
         bullets.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
     }
+
 }
